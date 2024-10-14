8a94124431671e600821c9ade14e1d1b674f3ee6
Dunlag <fernando.pinilla85@gmail.com>
2024-10-14 09:04:07 +0200

    first commit

---
+++ ej2/public/javascripts/offcanvas.js
@@ -0,0 +1,9 @@
+
+document.querySelector(".a__abrir").addEventListener("click", () => {
+    document.querySelector("aside.offcanvas").classList.toggle("show");
+});
+
+
+document.querySelector("close").addEventListener("click", () => {
+    document.querySelector("aside.offcanvas").classList.toggle("show");
+});
